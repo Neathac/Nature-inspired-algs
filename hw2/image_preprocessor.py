7179b592c91c8e349b29b3eb3829d203c964ad5d
Martin Gráf <Neath.graf@gmail.com.com>
2023-04-20 20:41:34 +0200

    Hw2 solution

--- hw2/image_preprocessor.py
+++ hw2/image_preprocessor.py
@@ -1,16 +1,13 @@
-import os 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+import PIL
+import os
+import os.path
+from PIL import Image
 
-path = 'dataset/weed'
+path = 'dataset/methamphetamine'
 
-training_data = []
-for img in os.listdir(path):
-    pic = cv2.imread(os.path.join(path,img))
-    if pic is None:
-        continue
-    pic = cv2.resize(pic,(128,128))
-    training_data.append([pic])
+for file in os.listdir(path):
+    f_img = path+"/"+file
+    img = Image.open(f_img)
+    img = img.resize((224,224))
+    img.save(f_img)
 
-np.save('processed_dataset/weed',np.array(training_data))
